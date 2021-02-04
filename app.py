8b522c717a155d1ce16182d31cd9388e3105007f
Eduardo <eduardo.lujan.p@gmail.com>
2021-02-04 16:46:36 -0600

    Added: Develop templates with forms

--- app.py
+++ app.py
@@ -10,42 +10,24 @@ app.permanent_session_lifetime = datetime.timedelta(days=365)
 app.secret_key = "hD4GbkeacT3KXuwZ"
 
 
-@app.route('/')
-def home():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
    """
    Home
    :return:
    :rtype:
    """
-   response_data = dict(
-      message="Eduardo"
-   )
-   return render_template("first_template.html", **response_data), 201
-
-
-@app.route('/edit-cookie', methods=['GET', 'POST'])
-def edit_cookie():
-   if request.method == 'GET':
-      return render_template("edit_cookie.html")
-
    if request.method == 'POST':
-      cookie_value = request.form.get('cookie-value')
-      session['cookie-value'] = cookie_value
-      return redirect('/cookie')
+      email = request.form.get('email')
+      password = request.form.get('password')
+      return redirect(f'/backoffice/?email={email}&password={password}')
+
+   return render_template("login.html"), 200
 
 
-@app.route('/cookie')
-def cookie_content():
-   """
-   Cookie content
-   :return:
-   :rtype:
-   """
-   cookies = request.cookies
-   resp = make_response(render_template('cookie_content.html', cookies=cookies))
-   cookie_value = session.get('cookie-value', '')
-   resp.set_cookie('cookie-value', cookie_value)
-   return resp
+@app.route('/backoffice')
+def backoffice(methods=['GET']):
+   return render_template("backoffice.html"), 200
 
 
 if __name__ == '__main__':
